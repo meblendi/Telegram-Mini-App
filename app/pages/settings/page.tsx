bcf7a0196bc446e0bd8d5ab4d881cd04014ef26b
meblendi <mhdsaffari@gmail.com>
2025-07-25 18:02:08 +0330

    Initial commit

--- app/pages/settings/page.tsx
+++ app/pages/settings/page.tsx
@@ -1,3 +1,16 @@
+import Link from "next/link";
+
 export default function Settings() {
-  return <div>Settings Page COMING SOON</div>;
+  return (
+    <main>
+      <div className="text-center mt-4">
+        <Link href="/">
+          <button className="bg-red-500 text-white py-2 px-4 rounded">
+            Back to Home
+          </button>
+        </Link>
+      </div>
+      <div>Settings Page COMING SOON</div>
+    </main>
+  );
 }
