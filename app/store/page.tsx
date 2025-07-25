4ed4c57642d751259e2a11a853e55741bc122bc6
meblendi <mhdsaffari@gmail.com>
2025-07-25 18:38:36 +0330

    Initial commit

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -4,7 +4,7 @@ export default function Store() {
   return (
     <main>
       
-      <div className="text-center mt-10">Store Page COMING SOON</div>
+      <div className="text-center mt-10">Store Page <br></br>COMING SOON</div>
       {/* Bottom Navigation */}
       <div className="fixed bottom-0 left-0 right-0 bg-white shadow-md flex justify-around py-3 rounded-t-3xl">
         {[
