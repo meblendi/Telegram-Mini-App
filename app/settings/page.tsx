4b56196ba1caa61f7ffce59c0f67c2d05c1c31ae
meblendi <mhdsaffari@gmail.com>
2025-07-25 18:34:55 +0330

    Initial commit

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -11,6 +11,19 @@ export default function Settings() {
         </Link>
       </div>
       <div className="text-center mt-10">Settings Page COMING SOON</div>
+      {/* Bottom Navigation */}
+      <div className="fixed bottom-0 left-0 right-0 bg-white shadow-md flex justify-around py-3 rounded-t-3xl">
+        {[
+          { label: "Home", icon: "🏠", href: "/" },
+          { label: "Upgrade", icon: "🌟", href: "/store" },
+          { label: "Settings", icon: "⚙️", href: "/settings" },
+        ].map(({ label, icon, href }) => (
+          <Link href={href} key={label} className="text-center text-sm">
+            <div className="text-xl">{icon}</div>
+            <div>{label}</div>
+          </Link>
+        ))}
+      </div>
     </main>
   );
 }
