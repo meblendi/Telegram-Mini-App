6c53d94395618ad8e633cda7e3d3a01f96eb9c6a
meblendi <mhdsaffari@gmail.com>
2025-08-02 18:24:37 +0330

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+//import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
