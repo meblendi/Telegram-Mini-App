6faf9610ba1e7b5a3892966b902e678c75466399
meblendi <mhdsaffari@gmail.com>
2025-08-02 17:50:50 +0330

    Initial commit

--- app/server.ts
+++ app/server.ts
@@ -1,1 +1,3 @@
-export const url = "https://newbi-django-render-app.onrender.com";
+export const url = "https://miniapp-django-vercel-7t5f.vercel.app";
+
+//export const url = "https://newbi-django-render-app.onrender.com";
